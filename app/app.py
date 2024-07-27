d5aa3c69862a48eca3b08c9575fdda5341d855e6
Jose Israel Rico Peña <jose.rico@laureate.mx>
2024-07-27 17:44:11 -0600

    Ajuste del Puerto 5001 intento 2

--- app/app.py
+++ app/app.py
@@ -8,10 +8,9 @@ def home():
     #return "¡Hola, mundo!"
     return render_template('home.html')
 
-
 @app.route('/about')
 def about():
     return "Acerca de nosotros"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
